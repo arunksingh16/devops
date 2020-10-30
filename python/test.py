e905961057d9cadbb7e261b1512575d0c0383362
Arun Kumar Singh <singh.arunk16@hotmail.com>
2020-10-30 12:12:05 +0000

    Create test.py

---
+++ python/test.py
@@ -0,0 +1,13 @@
+class FancyCar():
+  # Add a class variable...
+  wheels = 4
+  # Add a method
+  def driveFast(self):
+    print("Driving so fast")
+    
+# Instantiate a fancy car
+my_car = FancyCar()
+# Access the class attribute
+my_car.wheels4
+# Invoke the method
+my_car.driveFast()
