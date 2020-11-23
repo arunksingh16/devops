787466b3146efb6758f53273b0067397fc2cec25
Arun Kumar Singh <singh.arunk16@hotmail.com>
2020-11-23 18:17:22 +0000

    Update azurecmdlets.ps1

--- powershell/azurecmdlets.ps1
+++ powershell/azurecmdlets.ps1
@@ -7,3 +7,10 @@ Set-AzContext -SubscriptionId ""
 Get-AzADGroup -SearchString "developers"
 
 Get-AzADGroup -DisplayName "developers" | Format-List -Property Id
+
+# method 1 to fetch id 
+Get-AzADGroup -DisplayName "developers"  | Select -ExpandProperty Id
+
+# method 2 to fetch id
+$ObjId = (Get-AzADGroup -DisplayName "developers").Id
+Write-Host $ObjId
