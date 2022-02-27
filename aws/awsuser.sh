f097136a9a45c132b91e3d9603019017ffb6889f
Arun K Singh <22137632+arunksingh16@users.noreply.github.com>
2022-02-27 07:23:54 +0000

    Update awsuser.sh

--- aws/awsuser.sh
+++ aws/awsuser.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-$ aws iam create-user --user-name arun
-$ aws iam add-user-to-group --group-name FullAdministrator --user-name arun
-$ aws iam create-login-profile --user-name arun --password <pass> --password-reset-required
-$ aws iam create-access-key --user-name arun
+aws iam create-user --user-name arun
+aws iam add-user-to-group --group-name FullAdministrator --user-name arun
+aws iam create-login-profile --user-name arun --password <pass> --password-reset-required
+aws iam create-access-key --user-name arun
