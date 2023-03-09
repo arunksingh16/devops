295898fda1b17fb4cda02998bb1acec0b6ab0378
Arun K Singh <singh.arunk16@hotmail.com>
2023-03-09 21:36:52 +0000

    Update cloudwatch-cleanup-example.sh

--- aws/cloudwatch-cleanup-example.sh
+++ aws/cloudwatch-cleanup-example.sh
@@ -26,4 +26,29 @@ do
   fi
 done
 
+echo "Log group cleanup complete for app sync"
+echo "cleanup for lambda"
+
+
+# Loop through each log group and check if its associated AppSync API still exists
+for LOG_GROUP in $LOG_GROUPS
+do
+  # Extract the Function name from the log group name
+  F_name=$(echo $LOG_GROUP | sed "s|$LOG_GROUP_PREFIX||")
+
+  # Check if the function name with the extracted name still exists
+
+  F_EXISTS=$(aws lambda get-function --function-name $F_name --region $REGION --profile $PROFILE --query 'Configuration.FunctionName' --output text 2>/dev/null)
+  if [ -z "$F_EXISTS" ]
+  then
+    
+    echo "Deleting log group $LOG_GROUP..."
+    #aws logs describe-log-groups --log-group-name-prefix $LOG_GROUP --query 'logGroups[*].[logGroupName,storedBytes]' --profile $PROFILE --output text
+    #aws logs delete-log-group --log-group-name $LOG_GROUP --profile $PROFILE
+  else
+    echo "Keeping log group $LOG_GROUP..."
+  fi
+done
+
 echo "Log group cleanup complete."
+
